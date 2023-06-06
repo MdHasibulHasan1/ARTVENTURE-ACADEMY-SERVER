c7a24e947235fc816cb1076e8190e89abe03e2c7
hasib1511 <hasib7143@gmail.com>
2023-06-06 22:33:29 +0600

    make api to save  user data

--- index.js
+++ index.js
@@ -32,7 +32,26 @@ async function run() {
 
 
   
-    
+     // users apis
+     app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log(req.body);
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+
   
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
